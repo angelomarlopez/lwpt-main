29b87a1e75529d8e6e317dc02054f3a276e0d385
angelomarlopez <angel@aolopez.com>
2026-01-06 15:23:19 -0600

    Changed warning | timeout time to correct 15min to 20min

--- lwpt.cpp
+++ lwpt.cpp
@@ -89,7 +89,7 @@ void main_task( void* param ) {
       int delta = job.seconds_since_updated();
       
       if ( 
-        ( delta > 90 && delta < 120 ) &&
+        ( delta > 900 && delta < 1200 ) &&
         !job.is_warning_sent()
       ) {
         // TODO: Send HTTP Warning on 15min(900 sec)
@@ -97,7 +97,7 @@ void main_task( void* param ) {
         send_job_update( "warning" );
         job.set_warning_sent( true );
       } 
-      else if ( delta > 120 ) {
+      else if ( delta > 1200 ) {
         job.mark_timed_out();
 
         send_job_update( "timedout" );
